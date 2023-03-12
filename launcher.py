21bde54fb9ba0663014b5d6a1924a58d887b4bdf
timlr-jpg <119539145+timlr-jpg@users.noreply.github.com>
2023-03-12 23:30:05 +0100

    Cleaner code Launcher.py
    - Removed random unused time import
    - Removed unnecessary "get_user_input()" function
    - Changed "else" system to command based system
    - added Help command
    - added Message command
    - added "not recognized" command handling

--- launcher.py
+++ launcher.py
@@ -1,23 +1,31 @@
 from popbot import PopsauceClient
-from time import sleep
-def get_user_input():
-    return input("Enter message to send: ")
 
 if __name__ == "__main__":
     client = PopsauceClient("Mr.nobody", "PBKZ")
     client.joinRoom()
 
     while True:
-        # Get user input for message to send
-        message = get_user_input()
-
-        if message == "play":
+        # Get user input for command
+        print("For commands, type help")
+        Input = input(" ")
+        
+        if Input == "play":
             # Play a round
             client.playRound()
-        elif message == "join":
+            
+        elif Input == "help":
+            # Display commands and help
+            print("play -- Play a round of Popsauce")
+            print("join -- Join a room of Popsauce")
+            print("message -- After type in message when promted to send in chat")
+            
+        elif Input == "join":
             # Join room
             client.joinRound()
-        else:
-            # Send chat message
-            client.sendChat(message)
             
+        elif Input == "message":
+            msg = input("Enter message to send: ")
+            client.sendChat(msg)
+        
+        else:
+            print("'"+Input+"' is not a recognized command.")
